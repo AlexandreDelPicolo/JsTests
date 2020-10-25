c290ce31d9b6f122c006d8d630e71f95e8edc928
ALEXANDRE DEL PICOLO <60003491@midway.com.br>
2020-10-25 20:33:25 -0300

    Adicionada lib sinon e implementado dois testes com ela.

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,12 +1,36 @@
-
-const MyClass = require('../src/myClass.js');
-const myObjt = new MyClass();
+const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 
+const MyClass = require('../src/myClass.js');
+const myObjt = new MyClass();
 
 describe('Test suit', () => {
   it('Test the add method', () => {
     expect(myObjt.add(1, 2)).to.be.equal(3);
+  });
+
+  it('Spy the add method', () => {
+    const spy = sinon.spy(myObjt, 'add');
+    const arg1 = 10, arg2 = 20;
+    myObjt.callAnotherFunction(arg1, arg2);
+    //sinon.assert.calledOnce(spy);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith(arg1, arg2)).to.be.true;
+  });
+
+  it('Spy the callback method', () => {
+    const callback = sinon.spy();
+    myObjt.callTheCallback(callback);
+    expect(callback.calledOnce).to.be.true;
+  });
+
+  it('Mock the sayHello method', () => {
+    const mock = sinon.mock(myObjt);
+    const expectation = mock.expects('sayHello');
+    expectation.exactly(1);
+    expectation.withArgs('hello world');
+    myObjt.callAnotherFunction(10, 20);
+    mock.verify();
   });
 });
--- src/myClass.js
+++ src/myClass.js
@@ -3,10 +3,24 @@ class MyClass {
     console.log("Initiate");
   }
 
+  sayHello(str) {
+    console.log(str);
+  }
+
   add(arg1, arg2) {
     let result;
     result = arg1 + arg2;
     return result;
+  }
+
+  callAnotherFunction(arg1, arg2) {
+    this.sayHello('hello world');
+    const result = this.add(arg1, arg2);
+    return result;
+  }
+
+  callTheCallback(callback) {
+    callback();
   }
 }
 
