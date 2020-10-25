7da1a88359934d93093e530c0f195e4b1c33bf88
ALEXANDRE DEL PICOLO <60003491@midway.com.br>
2020-10-25 20:43:37 -0300

    Aprendendo stubs com sinon.

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 const MyClass = require('../src/myClass.js');
 const myObjt = new MyClass();
 
-describe('Test suit', () => {
+describe.skip('Test suit', () => {
   it('Test the add method', () => {
     expect(myObjt.add(1, 2)).to.be.equal(3);
   });
@@ -32,5 +32,16 @@ describe('Test suit', () => {
     expectation.withArgs('hello world');
     myObjt.callAnotherFunction(10, 20);
     mock.verify();
+  });
+});
+
+describe('Test suit for stub', () => {
+  it('Stub the add method', () => {
+    const stub = sinon.stub(myObjt, 'add');
+    stub.withArgs(10, 20)
+      .onFirstCall().returns(100)
+      .onSecondCall().returns(200);
+    expect(myObjt.callAnotherFunction(10, 20)).to.be.equal(100);
+    expect(myObjt.callAnotherFunction(10, 20)).to.be.equal(200);
   });
 });
